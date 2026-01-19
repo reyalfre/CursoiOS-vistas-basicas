d37f3429e5f2d1f715ced839b7a8bbf97a49e31e
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-01-19 10:14:42 +0100

    Initial Commit

---
+++ CursoiOS vistas basicas/CursoiOS_vistas_basicasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOS_vistas_basicasApp.swift
+//  CursoiOS vistas basicas
+//
+//  Created by Equipo 8 on 19/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOS_vistas_basicasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
